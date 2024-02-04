c8ad0d7e38a75195e04af23a6f1416d1dabb5571
spider777cloudy <pariket0703@gmail.com>
2024-02-03 22:50:41 -0500

    Lab3
    Lab3

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    Lab3 _inputs;
+    Vector2 _move;
+
+    [Header("Character Controller")]
+    [SerializeField] CharacterController _controller;
+
+    [Header("Movements")]
+    [SerializeField] float _speed;
+    [SerializeField] float _gravity = -30.0f;
+    [SerializeField] float _jumpHeight = 3.0f;
+    [SerializeField] Vector3 _velocity;
+
+    [Header("Ground Detection")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundRadius = 0.5f;
+    [SerializeField] LayerMask _groundMask;
+    [SerializeField] bool _isGrounded;
+
+    [Header("Respawn Locations")]
+    [SerializeField] Transform _respawn;
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _inputs = new Lab3();
+        _inputs.Player.Move.performed += context => _move = context.ReadValue<Vector2>();
+        _inputs.Player.Move.canceled += context => _move = Vector2.zero;
+        _inputs.Player.Jump.performed += context => Jump();
+    }
+
+    void OnEnable() => _inputs.Enable();
+
+    void OnDisable() => _inputs.Disable();
+
+    void FixedUpdate()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundRadius, _groundMask);
+        if (_isGrounded && _velocity.y < 0.0f)
+        {
+            _velocity.y = -2.0f;
+        }
+        Vector3 movement = new Vector3(_move.x, 0.0f, _move.y) * _speed * Time.fixedDeltaTime;
+        _controller.Move(movement);
+        _velocity.y += _gravity * Time.fixedDeltaTime;
+        _controller.Move(_velocity * Time.fixedDeltaTime);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_groundCheck.position, _groundRadius);
+    }
+    void Jump()
+    {
+        if (_isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2.0f * _gravity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Triggering with {other.gameObject.tag}");
+        if (other.CompareTag("DTzone"))
+        {
+            _controller.enabled = false;
+            transform.position = _respawn.position;
+            _controller.enabled = true;
+        }
+    }
+}
